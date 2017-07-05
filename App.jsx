fedf288f899f97aa064995c0b4da5972afa6ee39
Felician Popa <felician.popa@freshbyteinc.com>
2017-07-05 17:15:36 +0300

    Routing.

--- App.jsx
+++ App.jsx
@@ -1,50 +1,54 @@
-import React from 'react';
-
-class App extends React.Component {
-	constructor () {
-		super();
-		this.state = {
-			data:
-			[
-				{
-					component: 'First...',
-					id: 1
-				},
-
-				{
-					component: 'Second...',
-					id: 2
-				},
-
-				{
-					component: 'Third...',
-					id: 3
-				}
-			]
-		}
-
-
-	}
-
-	render() {
-		return (
-			<div>
-				{this.state.data.map((dynamicComponent, i) => <Content
-					key = {i} componentData = {dynamicComponent}/>)}
-			</div>
-		)
-	}
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter, Route, Link } from 'react-router-dom'
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+	      <div>
+	      	<Header />
+	    	<Main />
+	      </div>
+      </BrowserRouter>
+    )
+  }
 }
 
-class Content extends React.Component {
-	render() {
-		return (
-			<div>
-				<div>{this.props.componentData.component}</div>
-				<div>{this.props.componentData.id}</div>
-			</div>
-		)
-	}
-}
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/contact' component={Contact}/>
+    </Switch>
+  </main>
+)
+
+const Header = () => (
+	<div>
+        <ul>
+           <li><Link to='/'>Home</Link></li>
+           <li><Link to='/about'>About</Link></li>
+           <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+     </div>
+)
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+)
+const Home = () => (
+  <div>
+    <h1>Homepage</h1>
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+  </div>
+)
 
 export default App;
