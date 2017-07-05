6f64f721a6f7d883f51d6e281e357738c3e28d70
Felician Popa <felician.popa@freshbyteinc.com>
2017-07-05 15:03:09 +0300

    Keys.

--- App.jsx
+++ App.jsx
@@ -4,25 +4,33 @@ class App extends React.Component {
 	constructor () {
 		super();
 		this.state = {
-			data: 'Initial data ...'
+			data:
+			[
+				{
+					component: 'First...',
+					id: 1
+				},
+
+				{
+					component: 'Second...',
+					id: 2
+				},
+
+				{
+					component: 'Third...',
+					id: 3
+				}
+			]
 		}
 
-		this.updateState = this.updateState.bind(this)
-	}
 
-	updateState(e) {
-		this.setState({data: e.target.value});
 	}
 
 	render() {
 		return (
 			<div>
-				<input type="text" value = {this.state.data} onChange={this.updateState}/>
-				<h2>{this.state.data}</h2>
-
-
-				<Content myDataProp = {this.state.data}
-					updateStateProp = {this.updateState}></Content>
+				{this.state.data.map((dynamicComponent, i) => <Content
+					key = {i} componentData = {dynamicComponent}/>)}
 			</div>
 		)
 	}
@@ -32,8 +40,8 @@ class Content extends React.Component {
 	render() {
 		return (
 			<div>
-				<input type="text" value={this.props.myDataProp} onChange={this.props.updateStateProp}/>
-				<h3>{this.props.myDataProp}</h3>
+				<div>{this.props.componentData.component}</div>
+				<div>{this.props.componentData.id}</div>
 			</div>
 		)
 	}
