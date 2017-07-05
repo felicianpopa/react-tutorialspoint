ed6372fcf67632741cf3769c021da417a837f9e8
Felician Popa <felician.popa@freshbyteinc.com>
2017-07-05 11:31:43 +0300

    State and Props.

--- App.jsx
+++ App.jsx
@@ -13,11 +13,23 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            <h1>{this.state.header}</h1>
+            <Header headerProp= {this.state.header} /> {/* this gets the data from the state*/}
+        	<Header headerProp= "this data is added from a string"/>
             <h2>{this.state.content}</h2>
          </div>
       );
    }
+}
+
+// This class uses props
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.headerProp}</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
